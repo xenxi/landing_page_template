729a476db514041b6ee51d1cc631e02ed91e7fb9
xenxi <antoniom.diaz.moreno@gmail.com>
2022-03-06 07:52:07 +0100

    :lipstick: update view

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -7,8 +7,14 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
-      child: const Center(
-        child: Text('Home'),
+      padding: const EdgeInsets.all(12),
+      alignment: Alignment.center,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Text(
+          'Home',
+          style: Theme.of(context).textTheme.headline1,
+        ),
       ),
     );
   }
