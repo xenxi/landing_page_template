5af2e477ad387569fde22950225ca8afa88091b7
xenxi <antoniom.diaz.moreno@gmail.com>
2022-03-06 06:57:25 +0100

    :tada: batman!

---
+++ android/app/src/main/kotlin/com/example/landing_page_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.landing_page_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const LandingApp());
+
+class LandingApp extends StatelessWidget {
+  const LandingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Landing Page Template',
+      home: Container(),
+    );
+  }
+}
