e5c196c69dd751a2bb115dc1f781dc308054bd78
xenxi <antoniom.diaz.moreno@gmail.com>
2022-03-06 13:27:33 +0100

    :dizzy: update menu

--- lib/presentation/main_menu.dart
+++ lib/presentation/main_menu.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class MainMenu extends StatelessWidget {
+class MainMenu extends HookWidget {
   const MainMenu({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isOpen = useState(false);
+    final controller =
+        useAnimationController(duration: const Duration(milliseconds: 400));
     return MouseRegion(
       cursor: SystemMouseCursors.click,
-      child: Container(
-        width: 120,
-        height: 40,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-            color: Colors.black, borderRadius: BorderRadius.circular(6)),
-        child: Row(
-          children: const [
-            Text(
-              'Menú',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            Spacer(),
-            Icon(Icons.menu),
-          ],
+      child: GestureDetector(
+        onTap: () {
+          if (isOpen.value) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+
+          isOpen.value = !isOpen.value;
+        },
+        child: Container(
+          width: 120,
+          height: 40,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          decoration: BoxDecoration(
+              color: Colors.black, borderRadius: BorderRadius.circular(6)),
+          child: Row(
+            children: [
+              const Text(
+                'Menú',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              const Spacer(),
+              AnimatedIcon(
+                  icon: AnimatedIcons.menu_close, progress: controller),
+            ],
+          ),
         ),
       ),
     );
