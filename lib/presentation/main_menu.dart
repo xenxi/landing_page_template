d92c2b0fc5ebc0ac6566566889524031ef372eaa
xenxi <antoniom.diaz.moreno@gmail.com>
2022-03-06 13:12:42 +0100

    :lipstick: update menu

--- lib/presentation/main_menu.dart
+++ lib/presentation/main_menu.dart
@@ -7,21 +7,24 @@ class MainMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 120,
-      height: 40,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-          color: Colors.black, borderRadius: BorderRadius.circular(6)),
-      child: Row(
-        children: const [
-          Text(
-            'Menú',
-            style: TextStyle(color: Colors.white, fontSize: 18),
-          ),
-          Spacer(),
-          Icon(Icons.menu),
-        ],
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: Container(
+        width: 120,
+        height: 40,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+            color: Colors.black, borderRadius: BorderRadius.circular(6)),
+        child: Row(
+          children: const [
+            Text(
+              'Menú',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Spacer(),
+            Icon(Icons.menu),
+          ],
+        ),
       ),
     );
   }
